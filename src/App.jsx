145abd3a28c18299afd5c10856e67d5a0c1f866a
codeninja44 <elvisbeshel9@gmail.com>
2023-08-03 20:03:54 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
-        {/* <Header /> */}
+        <Header />
         <Routes>
           <Route path='/registration' element={<Registration />} />
           <Route path='/login' element={<Login />} />
