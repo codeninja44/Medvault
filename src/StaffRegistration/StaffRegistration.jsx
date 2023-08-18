92a1a34dff9ab72d7ab8d1f69a30473257f49f6e
codeninja44 <elvisbeshel9@gmail.com>
2023-08-18 13:09:04 +0100

    create Staff

--- src/StaffRegistration/StaffRegistration.jsx
+++ src/StaffRegistration/StaffRegistration.jsx
@@ -18,9 +18,27 @@ const StaffRegistration = () => {
    const nav = useNavigate()
    const [load, setLoad] = useState(false)
 
-   const staffDetails = { name, age, email, phoneNumber, password, confirmPassword, role, hospitalcode, photo }
+   // const staffDetails = { name, age, email, phoneNumber, password, confirmPassword, role, hospitalcode, photo }
 
-   const url = "https://medvault.onrender.com/api/createprofile/"
+   const url = "https://medvault.onrender.com/api/createprofile"
+   const token = JSON.parse(localStorage.getItem("token"))
+
+   // function register(e) {
+   //    e.preventDefault()
+   //    setLoad(true)
+   //    axios.post(url, staffDetails, { headers: { "Authorization": `Bearer ${token}` } })
+   //       .then(res => {
+   //          console.log(res);
+   //          //   setVerify(true);
+   //          setLoad(false)
+   //          nav('/staffDashboard')
+   //       })
+   //       .catch((err) => {
+   //          toast.error(err.response.data.message)
+   //          setLoad(false)
+   //          console.log("The error ", err)
+   //       })
+   // }
 
    const File = (e) => {
       const file = e.target.files[0]
@@ -28,31 +46,30 @@ const StaffRegistration = () => {
       console.log(file)
    }
 
-   // function registerStaff(e) {
-   //    e.preventDefault()
-   //    setLoad(true)
-   //    axios.post(url, staffDetails)
-   //       .then(res => {
-   //          setLoad(false)
-   //          console.log(res)
-   //          nav = ('/staffDashboard')
-   //       })
-   //       .catch((err) => console.log('this is an error', err));
-   // }
-
+   const data = new FormData()
+   data.append("name", name)
+   data.append("age", age)
+   data.append("email", email)
+   data.append("phoneNumber", phoneNumber)
+   data.append("password", password)
+   data.append("confirmPassword", confirmPassword)
+   data.append("role", role)
+   data.append("hospitalcode", hospitalcode)
+   data.append("photo", photo)
 
 
    function register(e) {
       e.preventDefault()
       setLoad(true)
-      axios.post(url, staffDetails, {
+      axios.post(url, data, {
          header: { "Content-type": "multipart/form-data" }
       })
          .then(res => {
             console.log(res);
-            //   setVerify(true);
-            setLoadState(false)
+            // setVerify(true); 
+            setLoad(false)
             nav('/staffDashboard')
+
          })
          .catch((err) => {
             toast.error(err.response.data.message)
@@ -62,35 +79,39 @@ const StaffRegistration = () => {
    }
 
 
+
+
    return (
 
       <>
          <body>
-            <div className='staffreghead'>
-               <div className='stafflogowrapper'>
-                  <img src="./src/assets/logo.png" alt="logo" />
+            <form className="inputSec" onSubmit={register}>
+               <div className='staffreghead'>
+                  <div className='stafflogowrapper'>
+                     <img src="./src/assets/logo.png" alt="logo" />
+                  </div>
                </div>
-            </div>
-            <div className='staffregcarddiv'>
-               <div className='staffregcard'>
-                  <div className='staffregtext'>Staff Registration</div>
-                  <div className='staffregcardholder'>
-                     <input className='staffname' type="text" placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
-                     <input className='staffphoneno' type="text" placeholder='Phone No' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-                     <input className='staffemail' type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                     <input className='staffemail' type="text" placeholder='Age' value={age} onChange={(e) => setAge(e.target.value)} />
-                     <input type='file' accept='/image*/' placeholder='Facility logo' className="inputType" onChange={File} />
-                     <input className='staffpassword' type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                     <input className='staffconfirmpass' type="password" placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-                     <input className='staffrole' type="text" placeholder='Role' value={role} onChange={(e) => setRole(e.target.value)} />
-                     <input className='staffhoscode' type="text" placeholder='Hospital Code' value={hospitalcode} onChange={(e) => setHospitalcode(e.target.value)} />
-                     <button className='staffregbutton' onClick={register}>{load ? "Loading..." : "Register"}</button>
-                     <div className='staffregdescription'>
-                        <p className='staffalready'>Already have an account? <span className='stafflogin' >Log In</span></p>
+               <div className='staffregcarddiv'>
+                  <div className='staffregcard'>
+                     <div className='staffregtext'>Staff Registration</div>
+                     <div className='staffregcardholder'>
+                        <input className='staffname' type="text" placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+                        <input className='staffphoneno' type="text" placeholder='Phone No' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                        <input className='staffemail' type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input className='staffemail' type="text" placeholder='Age' value={age} onChange={(e) => setAge(e.target.value)} />
+                        <input type='file' accept='/image*/' placeholder='Facility logo' className="inputType" onChange={File} />
+                        <input className='staffpassword' type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <input className='staffconfirmpass' type="password" placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                        <input className='staffrole' type="text" placeholder='Role' value={role} onChange={(e) => setRole(e.target.value)} />
+                        <input className='staffhoscode' type="text" placeholder='Hospital Code' value={hospitalcode} onChange={(e) => setHospitalcode(e.target.value)} />
+                        <button className='staffregbutton' type='submit'>{load ? "Loading..." : "Register"}</button>
+                        <div className='staffregdescription'>
+                           <p className='staffalready'>Already have an account? <span className='stafflogin' >Log In</span></p>
+                        </div>
                      </div>
                   </div>
                </div>
-            </div>
+            </form>
          </body>
       </>
    )
---
+++ src/createPatients/CreatePatients.jsx
@@ -0,0 +1,119 @@
+// import React, { useState } from 'react'
+// import './StaffRegistration.css'
+// import './StaffRegistrationmobile.css'
+// import axios from 'axios'
+// import { useNavigate } from 'react-router-dom'
+// import { toast } from 'react-hot-toast'
+
+const PatientsRegistration = () => {
+//     const [name, setName] = useState('')
+//     const [age, setAge] = useState('')
+//     const [email, setEmail] = useState('')
+//     const [phoneNumber, setPhoneNumber] = useState('')
+//     const [password, setPassword] = useState('')
+//     const [confirmPassword, setConfirmPassword] = useState('')
+//     const [role, setRole] = useState('')
+//     const [hospitalcode, setHospitalcode] = useState('')
+//     const [photo, setPhoto] = useState('')
+//     const nav = useNavigate()
+//     const [load, setLoad] = useState(false)
+
+//     // const staffDetails = { name, age, email, phoneNumber, password, confirmPassword, role, hospitalcode, photo }
+
+//     const url = "https://medvault.onrender.com/api/createprofile"
+//     const token = JSON.parse(localStorage.getItem("token"))
+
+//     // function register(e) {
+//     //    e.preventDefault()
+//     //    setLoad(true)
+//     //    axios.post(url, staffDetails, { headers: { "Authorization": `Bearer ${token}` } })
+//     //       .then(res => {
+//     //          console.log(res);
+//     //          //   setVerify(true);
+//     //          setLoad(false)
+//     //          nav('/staffDashboard')
+//     //       })
+//     //       .catch((err) => {
+//     //          toast.error(err.response.data.message)
+//     //          setLoad(false)
+//     //          console.log("The error ", err)
+//     //       })
+//     // }
+
+//     const File = (e) => {
+//         const file = e.target.files[0]
+//         setPhoto(file)
+//         console.log(file)
+//     }
+
+//     const data = new FormData()
+//     data.append("name", name)
+//     data.append("age", age)
+//     data.append("email", email)
+//     data.append("phoneNumber", phoneNumber)
+//     data.append("password", password)
+//     data.append("confirmPassword", confirmPassword)
+//     data.append("role", role)
+//     data.append("hospitalcode", hospitalcode)
+//     data.append("photo", photo)
+
+
+//     function register(e) {
+//         e.preventDefault()
+//         setLoad(true)
+//         axios.post(url, data, {
+//             header: { "Content-type": "multipart/form-data" }
+//         })
+//             .then(res => {
+//                 console.log(res);
+//                 // setVerify(true);
+//                 setLoad(false)
+//                 nav('/staffDashboard')
+
+//             })
+//             .catch((err) => {
+//                 toast.error(err.response.data.message)
+//                 setLoad(false)
+//                 console.log("The error ", err)
+//             })
+//     }
+
+
+
+
+//     return (
+
+//         <>
+//             <body>
+//                 <form className="inputSec" onSubmit={register}>
+//                     <div className='staffreghead'>
+//                         <div className='stafflogowrapper'>
+//                             <img src="./src/assets/logo.png" alt="logo" />
+//                         </div>
+//                     </div>
+//                     <div className='staffregcarddiv'>
+//                         <div className='staffregcard'>
+//                             <div className='staffregtext'>Staff Registration</div>
+//                             <div className='staffregcardholder'>
+//                                 <input className='staffname' type="text" placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+//                                 <input className='staffphoneno' type="text" placeholder='Phone No' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+//                                 <input className='staffemail' type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+//                                 <input className='staffemail' type="text" placeholder='Age' value={age} onChange={(e) => setAge(e.target.value)} />
+//                                 <input type='file' accept='/image*/' placeholder='Facility logo' className="inputType" onChange={File} />
+//                                 <input className='staffpassword' type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+//                                 <input className='staffconfirmpass' type="password" placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+//                                 <input className='staffrole' type="text" placeholder='Role' value={role} onChange={(e) => setRole(e.target.value)} />
+//                                 <input className='staffhoscode' type="text" placeholder='Hospital Code' value={hospitalcode} onChange={(e) => setHospitalcode(e.target.value)} />
+//                                 <button className='staffregbutton' type='submit'>{load ? "Loading..." : "Register"}</button>
+//                                 <div className='staffregdescription'>
+//                                     <p className='staffalready'>Already have an account? <span className='stafflogin' >Log In</span></p>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </form>
+        </>
+    )
+}
+
+export default PatientsRegistration
